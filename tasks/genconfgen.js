d5e4d0858193542e3cbc8be4aa72e59d6b540d9a
bullgare <bullgare@gmail.com>
2013-12-05 18:14:18 +0400

    Initial commit

---
+++ tasks/genconfgen.js
@@ -0,0 +1,8 @@
+"use strict";
+module.exports = function (grunt) {
+	var ConfGenerator = require('../lib/generator.js');
+
+	grunt.registerMultiTask('genconfgen', 'Generates custom configs with provided config generators', function () {
+
+	});
+};
